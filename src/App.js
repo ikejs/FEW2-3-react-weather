29a729cf946c8f4f79616f60bd92b0afa13b01bd
Ike <ikescode@gmail.com>
2020-04-17 01:24:14 -0500

    update gitignore

--- src/App.js
+++ src/App.js
@@ -31,31 +31,27 @@ class App extends Component {
     }
   }
 
+  async getData(url) {
+    try {
+      const res = await fetch(url);
+      const json = await res.json();
+      return json;
+    } catch(err) {
+      return err
+    }
+  }
+
   handleSubmit(e) {
     e.preventDefault()
-    // ! Get your own API key ! 
     const apikey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY
-    // Get the zip from the input
     const zip = this.state.inputValue
-    // Form an API request URL with the apikey and zip
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apikey}`
-    // Get data from the API with fetch
-    fetch(url).then(res => {
-      // Handle the response stream as JSON
-      return res.json()
-    }).then((json) => {
-      // If the request was successful assign the data to component state
-      this.setState({ weatherData: json })
-      // ! This needs better error checking here or at renderWeather() 
-      // It's possible to get a valid JSON response that is not weather 
-      // data, for example when a bad zip code entered.
+    this.getData(url).then(json => {
+      this.setState({ weatherData: json });
     }).catch((err) => {
-      // If there is no data 
       this.setState({ weatherData: null }) // Clear the weather data we don't have any to display
-      // Print an error to the console. 
       console.log('-- Error fetching --')
       console.log(err.message)
-      // You may want to display an error to the screen here. 
     })
   }
 
